4fdd40965a93541ee1f636c91058a18574f834cc
utkirkurbanov <utkirkurbanov2700@gmail.com>
2023-03-25 00:23:57 +0500

    Created testClass.jsx and worked with Class and functional components and props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
 import './App.css';
-import Test from './components/test';
+import Test2 from './components/test2';
+
+
 function App() {
+  const expences = [
+    {
+      title: "Dasturlash kurslari",
+      amount: 35,
+      date: new Date(2023, 3, 25)
+    },
+    {
+      title: "Kammunal harajatlar",
+      amount: 20,
+      date: new Date(2023, 3, 25)
+    },
+    {
+      title: "Yo'l kira",
+      amount: 11,
+      date: new Date(2023, 3, 25)
+    },
+    {
+      title: "Ijara",
+      amount: 100,
+      date: new Date(2023, 5, 25)
+    },
+    {
+      title: "Kutilmagan harajatlar",
+      amount: 45,
+      date: new Date(2023, 3, 25)
+    },
+  ];
+
   return (
-    <div>
+    <div className='container'>
       <h1 className='headline'>Hello World</h1>
-      <h2>Front-End Developer Roadmap</h2>
-      <Test />
+      <h2>Front-end dasturchining bir oylik xarajatlari</h2>
+      <Test2
+        title={
+          expences[0].title  
+        }
+        amount={
+          expences[0].amount
+        }
+        date={expences[0].date}
+      />
+      <Test2
+        title={
+          expences[1].title  
+        }
+        amount={
+          expences[1].amount
+        }
+        date={expences[1].date}
+      />
+      <Test2
+        title={
+          expences[2].title  
+        }
+        amount={
+          expences[2].amount
+        }
+        date={expences[2].date}
+      />
+      <Test2
+        title={
+          expences[3].title  
+        }
+        amount={
+          expences[3].amount
+        }
+        date={expences[3].date}
+      />
+      <Test2
+        title={
+          expences[4].title  
+        }
+        amount={
+          expences[4].amount
+        }
+        date={expences[4].date}
+      />
     </div>
   );
 }
--- src/components/test2.jsx
+++ src/components/test2.jsx
@@ -1,6 +1,17 @@
-function test2() {
+import React from "react";
+
+function test2(props) {
+    // const expenseDate = new Date(2023, 3, 24);
+    // const expenseTitle = 'Front-End Course';
+    // const expenseAmount = 400000;
+
     return (
-        <div>Test2</div>
+        <div>
+            Date: {props.date.toISOString()}
+            <h2>Reason: {props.title}</h2>
+            <p>Cost: ${props.amount} US</p>
+        </div>
+
     )
 }
 export default test2;
