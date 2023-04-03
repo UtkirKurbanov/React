2fb3bec8461ea001a0e60fd6610d8fb5ab0bb571
utkirkurbanov <utkirkurbanov2700@gmail.com>
2023-04-04 01:01:19 +0500

    lesson6 working with useState

---
+++ src/components/counter.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+
+    const addNumberHandler = () => {
+        setCounter(counter + 1);
+    }
+    return (
+        <div>
+            <p>{counter}</p>
+          <button onClick={addNumberHandler}>Add
+          </button>
+    </div>
+  )
+}
+
+export default Counter;
